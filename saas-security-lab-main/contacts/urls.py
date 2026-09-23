1a51dee7edaed5ef2f146a23eff0052b3d143906
lxrider <182007340+lxrider@users.noreply.github.com>
2026-09-23 20:22:36 +0200

    Delete saas-security-lab-main directory

--- saas-security-lab-main/contacts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.contact_list, name="contact-list"),
-    path("<int:pk>/", views.contact_detail, name="contact-detail"),
-]
